5d3d39f67ac8d9a7512060418e035c8e5103e04a
sarathanatarajan <sarathanatarajan17@gmail.com>
2023-01-06 10:31:25 +0530

    Adding  Gradle project

---
+++ src/main/java/Demo_Gradle_Project/HelloWorld.java
@@ -0,0 +1,11 @@
+package Demo_Gradle_Project;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo - Gradle ");
+
+	}
+
+}
